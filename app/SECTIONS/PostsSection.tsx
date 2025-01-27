af8a49ff8006b96ef637cc9b16add827d231c823
Ismail <Gaismail777@gmail.com>
2025-01-27 22:09:11 +0500

    changes

--- app/SECTIONS/PostsSection.tsx
+++ app/SECTIONS/PostsSection.tsx
@@ -1,94 +1,102 @@
-"use client"
-import React, { useState } from 'react';
-import { posts } from '@/data/posts';
-import { LikeIcon } from '@/public/icons';
+"use client";
+import React, { useState } from "react";
+import { posts } from "@/data/posts";
+import { LikeIcon } from "@/public/icons";
 
 const PostsSection = () => {
-    const [likedPosts, setLikedPosts] = useState<Set<string>>(new Set());
+  const [likedPosts, setLikedPosts] = useState<Set<string>>(new Set());
 
-    const handleLike = (postId: string) => {
-        setLikedPosts(prev => {
-            const newSet = new Set(prev);
-            newSet.has(postId) ? newSet.delete(postId) : newSet.add(postId);
-            return newSet;
-        });
-    };
+  const handleLike = (postId: string) => {
+    setLikedPosts((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(postId)) {
+        newSet.delete(postId);
+      } else {
+        newSet.add(postId);
+      }
+      return newSet;
+    });
+  };
 
-    const formatDate = (date: Date) => {
-        return new Intl.DateTimeFormat('en-GB', {
-            day: '2-digit',
-            month: 'short',
-            year: 'numeric'
-        }).format(date);
-    };
-    return (
-        <div className="flex flex-col ">
-            <div className="mt-[15px] ml-[20px] flex self-start items-center justify-center rounded-[45px] h-[45px] px-[15px] min-w-[111px] gap-[25px] dark:text-[#DADADA] text-[black] dark:bg-[#2D2C2C]">
-                New
-                {/* ARROW DOWn */}
-                <div className="relative block">
-                    <span className="border-[1px] w-[12px] rounded-[20px] absolute -translate-x-[55%] right-[50%] top-[50%] translate-y-[-50%] rotate-[50deg]  bg-[#9B9C9C]"></span>
-                    <span className="border-[1px] w-[12px] rounded-[20px] absolute translate-x-[0%] right-[50%] top-[50%] translate-y-[-50%] rotate-[130deg]  bg-[#9B9C9C]"></span>
-                </div>
-            </div>
-            <div className="px-[25px] mt-[15px]">
-                {posts.map((item) => {
-                    const isActive = likedPosts.has(item.id);
-                    const formattedDate = formatDate(item.date);
-
-                    return (
-                        <div key={item.title} className="p-[20px] flex flex-col justify-between dark:bg-dark_theme_bg w-full rounded-[20px] max-w-[641px] mb-4">
-                            <div className="flex flex-col gap-[15px] w-full">
-                                <div className="flex">
-                                    <div className="flex flex-col w-full gap-[5px]">
-                                        <div className="flex items-center gap-[10px]">
-                                            {/* Tags */}
-                                            {item.tags.map((tag, index) => (
-                                                <div key={index} className="flex items-center px-[10px] py-[1px] gap-[5px] dark:text-white bg-[#9B9C9C15] rounded-[25px]">
-                                                    <span>#</span>
-                                                    <p>{tag}</p>
-                                                </div>
-                                            ))}
-                                            {/* Formatted Date */}
-                                            <span className="text-sm dark:text-[#DADADA]">
-                                                {formattedDate}
-                                            </span>
-                                        </div>
-                                        {/* Post Content */}
-                                        <h2 className="text-[25px] font-bold dark:text-white leading-[34px]">
-                                            {item.title}
-                                        </h2>
-                                        <p className="text-gray-600 dark:text-gray-300 line-clamp-4 max-w-[417px] leading-[25px]">
-                                            {item.descr}
-                                        </p>
-                                    </div>
-                                    {/* IMAGE CONTENT */}
-                                    <div className="max-w-[166px] h-[172px] w-full bg-gray-500 rounded-[10px]"></div>
-                                </div>
-                                {/* TEXT CONTENT */}
-
-                                {/* LIKES COMMENTS CONTENT */}
-                                <div className="flex justify-between w-full">
-                                    {/* LIKE BUTTON */}
-                                    <div className="flex items-center gap-[5px] cursor-pointer select-none text-white" onClick={() => handleLike(item.id)}>
-                                        <LikeIcon
-                                            isActive={isActive}
-
-                                        />
-                                        {item.likes}
-                                    </div>
-                                    {/* COMMENT BUTTON */}
-                                    <div className=""></div>
-                                </div>
-                            </div>
-
-                        </div>
-                    );
-                })}
-            </div>
+  const formatDate = (date: Date) => {
+    return new Intl.DateTimeFormat("en-GB", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    }).format(date);
+  };
+  return (
+    <div className="flex flex-col ">
+      <div className="mt-[15px] ml-[20px] flex self-start items-center justify-center rounded-[45px] h-[45px] px-[15px] min-w-[111px] gap-[25px] dark:text-[#DADADA] text-[black] dark:bg-[#2D2C2C]">
+        New
+        {/* ARROW DOWn */}
+        <div className="relative block">
+          <span className="border-[1px] w-[12px] rounded-[20px] absolute -translate-x-[55%] right-[50%] top-[50%] translate-y-[-50%] rotate-[50deg]  bg-[#9B9C9C]"></span>
+          <span className="border-[1px] w-[12px] rounded-[20px] absolute translate-x-[0%] right-[50%] top-[50%] translate-y-[-50%] rotate-[130deg]  bg-[#9B9C9C]"></span>
         </div>
-    );
+      </div>
+      <div className="px-[25px] mt-[15px]">
+        {posts.map((item) => {
+          const isActive = likedPosts.has(item.id);
+          const formattedDate = formatDate(item.date);
 
+          return (
+            <div
+              key={item.title}
+              className="p-[20px] flex flex-col justify-between dark:bg-dark_theme_bg w-full rounded-[20px] max-w-[50vw] mb-4"
+            >
+              <div className="flex flex-col gap-[15px] w-full">
+                <div className="flex gap-[30px]">
+                  <div className="flex flex-col w-full gap-[5px]">
+                    <div className="flex items-center gap-[10px]">
+                      {/* Tags */}
+                      {item.tags.map((tag, index) => (
+                        <div
+                          key={index}
+                          className="flex items-center px-[10px] py-[1px] gap-[5px] dark:text-white bg-[#9B9C9C15] rounded-[25px]"
+                        >
+                          <span>#</span>
+                          <p>{tag}</p>
+                        </div>
+                      ))}
+                      {/* Formatted Date */}
+                      <span className="text-sm dark:text-[#DADADA]">
+                        {formattedDate}
+                      </span>
+                    </div>
+                    {/* Post Content */}
+                    <h2 className="text-[25px] font-bold dark:text-white leading-[34px]">
+                      {item.title}
+                    </h2>
+                    <p className="text-gray-600 dark:text-gray-300 line-clamp-4 max-w-[40vw] leading-[25px]">
+                      {item.descr}
+                    </p>
+                  </div>
+                  {/* IMAGE CONTENT */}
+                  <div className="max-w-[166px] h-[172px] w-full bg-gray-500 rounded-[10px]"></div>
+                </div>
+                {/* TEXT CONTENT */}
+
+                {/* LIKES COMMENTS CONTENT */}
+                <div className="flex justify-between w-full">
+                  {/* LIKE BUTTON */}
+                  <div
+                    className="flex items-center gap-[5px] cursor-pointer select-none text-white"
+                    onClick={() => handleLike(item.id)}
+                  >
+                    <LikeIcon isActive={isActive} />
+                    {item.likes}
+                  </div>
+                  {/* COMMENT BUTTON */}
+                  <div className=""></div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default PostsSection;
--- components/main/Header.tsx
+++ components/main/Header.tsx
@@ -1,38 +1,45 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Logo from "@/public/svg/Logo.svg"
-import search_icon from "@/public/svg/search-icon.svg"
-import notificaton from "@/public/svg/notification.svg"
-import { Nunito } from 'next/font/google'
-import ThemeToggle from '../buttons/ThemeToggle'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "@/public/svg/Logo.svg";
+import search_icon from "@/public/svg/search-icon.svg";
+import notificaton from "@/public/svg/notification.svg";
+import { Nunito } from "next/font/google";
+import ThemeToggle from "../buttons/ThemeToggle";
 
-const nunito = Nunito({ subsets: ["latin"], weight: "600" })
+const nunito = Nunito({ subsets: ["latin"], weight: "600" });
 
 const Header = () => {
-    return (
-        <header className="fixed top-0 left-0 w-full z-50 bg-light_theme_body dark:bg-dark_theme_body h-[70px] flex items-center border-b-[1px] dark:border-[#3E4142]">
-            <div className="flex justify-between w-full mx-auto max-w-[1440px] md:px-[35px] sm:px-[25px] px-[20px]">
-                <Link href="/" className='flex items-center'>
-                    <Image src={Logo} alt="" />
-                </Link>
-                <div className="h-[40px] min-w-[380px] dark:bg-white bg-[#B0B0B0] text-black flex items-center rounded-[40px] px-[16px] py-[12px] gap-[12px]">
-                    <Image src={search_icon} alt="" className='w-[16px] h-[16px]' />
-                    <input type="text" className="w-full dark:bg-white bg-[#B0B0B0] outline-none border-none dark:placeholder-[#00000063] placeholder-[#00000080]" placeholder="Search Hakkaten" />
-                </div>
+  return (
+    <header className="fixed top-0 left-0 w-full z-50 bg-light_theme_body dark:bg-dark_theme_body h-[70px] flex items-center border-b-[1px] dark:border-[#3E4142]">
+      <div className="flex justify-between w-full mx-auto md:px-[35px] sm:px-[25px] px-[20px]">
+        <Link href="/" className="flex items-center">
+          <Image src={Logo} alt="" />
+        </Link>
+        <div className="h-[40px] min-w-[380px] dark:bg-white bg-[#B0B0B0] text-black flex items-center rounded-[40px] px-[16px] py-[12px] gap-[12px]">
+          <Image src={search_icon} alt="" className="w-[16px] h-[16px]" />
+          <input
+            type="text"
+            className="w-full dark:bg-white bg-[#B0B0B0] outline-none border-none dark:placeholder-[#00000063] placeholder-[#00000080]"
+            placeholder="Search Hakkaten"
+          />
+        </div>
 
-                <ThemeToggle />
-                <div className="flex items-center gap-[15px]">
-                    <Link href="/notifications">
-                        <Image src={notificaton} alt="" />
-                    </Link>
-                    <Link href="/login" className={`${nunito.className} text-[16px] leading-[22px] w-[80px] h-[40px] rounded-[40px] flex items-center justify-center text-center bg-primary`}>
-                        Log in
-                    </Link>
-                </div>
-            </div>
-        </header>
-    )
-}
+        <ThemeToggle />
+        <div className="flex items-center gap-[15px]">
+          <Link href="/notifications">
+            <Image src={notificaton} alt="" />
+          </Link>
+          <Link
+            href="/login"
+            className={`${nunito.className} text-[16px] leading-[22px] w-[80px] h-[40px] rounded-[40px] flex items-center justify-center text-center bg-primary`}
+          >
+            Log in
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;

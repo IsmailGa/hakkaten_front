063f120285dca11a8460a06ee0b4c9d65c32fa3f
Isa <129764901+IsmailGa@users.noreply.github.com>
2025-01-29 00:10:16 +0500

    Update layout.tsx

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import { Nunito } from "next/font/google";
 
 const nunito = Nunito({ subsets: ["latin"], weight: "400" });
@@ -8,7 +7,6 @@ export default function Layout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const theme = cookies().get('theme')?.value || 'dark';
     return (
         <div className={`${nunito.className} antialiased bg-light_theme_body dark:bg-dark_theme_body text-white`}>
             <main className="flex items-center justify-center w-full h-screen">
